de12842d22637dfe6cc577e5a46efd454a95c894
Angela Csomo <angela.csomo@gmail.com>
2020-12-10 09:30:09 +0100

    1_scope and steps

---
+++ script.js
@@ -0,0 +1,41 @@
+function pageLoad() {
+
+  //toDo applikációt készítünk. Alapértelmezésben 10 üres mező áll rendelkezésre, amelyhez szabadon hozzáadhatóak új mezők is.
+  //Minden mezőbe bepipálhatjuk, hogy készen vagyunk. Van input mező és van egyéb pipálási lehetőség és törlési gomb
+
+  //1. az id=root-ba egy változót definiálok
+
+
+  //2. eltároljuk egy változóba a minimum sorok számát (10) , mert nincs tömbünk.
+
+
+  //3. egy változóba elmentjük a html egy során div sort chekcbox, text input és törlés gomb
+
+
+  //4. tízszer megismételjük a 3. lépést for ciklussal és hozzáadjuk a html root div-hez
+
+
+  //5. Listán kívülre egy törlési gomb (insertAdjecentHTML afterend)
+
+
+  //6. A gombot működésre bírjuk: hozzáadjuk a click eseményt.addEventListener.click-kel
+
+
+  //7. kijelölöm className alapján az összes törlés gombot és elmentem egy változóba. querySelectorAll
+
+
+  //8. minden törlés gombra ráteszünk egy click eseményt (for ciklus)
+
+
+  //9. Melyik gomb melyik soroz tartozik: Parentjének az outerHTML a kijelölése
+
+
+
+
+
+
+
+
+
+}
+window.addEventListener("load", pageLoaded);
